31e7cd7d409398b2c97a64ab7e8a7db02ecafa81
Arturs Narnickis <ki4kis@gmail.com>
2021-12-02 17:11:17 +0200

    Cleanup Day2

--- src/main/scala/coa_2021/Day2.scala
+++ src/main/scala/coa_2021/Day2.scala
@@ -5,36 +5,46 @@ import cats.effect.IO
 import Day2._
 
 object Day2Part1 extends AdventApp[Command](2) {
+  private val initPos = Pos(0, 0)
+
   def task(input: List[Command]): IO[Unit] = IO {
-    val (hor, depth) = input.foldLeft((0, 0)) {
-      case ((hor, depth), Command(Direction.Forward, units)) => (hor + units, depth)
-      case ((hor, depth), Command(Direction.Down, units)) => (hor, depth + units)
-      case ((hor, depth), Command(Direction.Up, units)) => (hor, depth - units)
-    }
+    val Pos(hor, depth) = input.foldLeft(initPos)(_ move _)
 
     println(s"($hor, $depth) = ${hor * depth}")
+  }
+
+  case class Pos(hor: Int, depth: Int) {
+    def move(cmd: Command): Pos = cmd match {
+      case Command.Forward(units) => copy(hor = hor + units)
+      case Command.Down(units) => copy(depth = depth + units)
+      case Command.Up(units) => copy(depth = depth - units)
+    }
   }
 }
 
 object Day2Part2 extends AdventApp[Command](2) {
+  private val initPos = Pos(0, 0, 0)
+
   def task(input: List[Command]): IO[Unit] = IO {
-    val (hor, depth, _) = input.foldLeft((0, 0, 0)) {
-      case ((hor, depth, aim), Command(Direction.Forward, units)) => (hor + units, depth + (aim * units), aim)
-      case ((hor, depth, aim), Command(Direction.Down, units)) => (hor, depth, aim + units)
-      case ((hor, depth, aim), Command(Direction.Up, units)) => (hor, depth, aim - units)
-    }
+    val Pos(hor, depth, _) = input.foldLeft(initPos)(_ move _)
 
     println(s"($hor, $depth) = ${hor * depth}")
+  }
+
+  case class Pos(hor: Int, depth: Int, aim: Int) {
+    def move(cmd: Command): Pos = cmd match {
+      case Command.Forward(units) => copy(hor + units, depth + (aim * units))
+      case Command.Down(units) => copy(aim = aim + units)
+      case Command.Up(units) => copy(aim = aim - units)
+    }
   }
 }
 
 object Day2 {
-  sealed trait Direction
-  object Direction {
-    case object Forward extends Direction
-    case object Down extends Direction
-    case object Up extends Direction
+  sealed trait Command { def units: Int }
+  object Command {
+    case class Forward(units: Int) extends Command
+    case class Down(units: Int) extends Command
+    case class Up(units: Int) extends Command
   }
-
-  case class Command(direction: Direction, units: Int)
 }
