6ee337eb6520191e208b75c931f7c4d8eb86c3c7
ki4kis <ki4kis@gmail.com>
2022-12-06 02:13:38 +0200

    Add tests for year 2022

---
+++ 2022/src/test/scala/aoc/y2022/Advent2022Spec.scala
@@ -0,0 +1,48 @@
+package aoc.y2022
+
+import common.v2.AdventApp
+import aoc.y2022.week1._
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.Assertions._
+
+import cats.effect.unsafe.implicits.global
+
+class Advent2022Spec extends AnyFreeSpec {
+  val apps: List[AdventApp[_]] = List(Day1, Day2, Day3, Day4, Day5)
+  val test: List[(Any, Any)] = List(
+    (24000, 45000),
+    (15, 12),
+    (157, 70),
+    (2, 4),
+    ("CMZ", "MCD")
+  )
+  val real: List[(Any, Any)] = List(
+    (71780, 212489),
+    (8890, 10238),
+    (8039, 2510),
+    (485, 857),
+    ("FWNSHLDNZ", "RNRGDNFQG")
+  )
+
+  "Advent of Code" - {
+    apps.zipWithIndex foreach { case (app, idx) =>
+      s"Year ${app.year} Day #${app.day}" - {
+        test.lift(idx) foreach { case (exp1, exp2) =>
+          "test" - {
+            val (act1, act2) = app.test.unsafeRunSync()
+            "part1" in assert(act1 == exp1)
+            "part2" in assert(act2 == exp2)
+          }
+        }
+        real.lift(idx) foreach { case (exp1, exp2) =>
+          "real" - {
+            val (act1, act2) = app.real.unsafeRunSync()
+            "part1" in assert(act1 == exp1)
+            "part2" in assert(act2 == exp2)
+          }
+        }
+      }
+    }
+  }
+}
