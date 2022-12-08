63e1564d04f60d9cd432061bac177d116a18d28c
ki4kis <ki4kis@gmail.com>
2022-12-08 13:19:37 +0200

    Small refactoring on Day8

--- 2022/src/main/scala/aoc/y2022/week2/Day8.scala
+++ 2022/src/main/scala/aoc/y2022/week2/Day8.scala
@@ -14,14 +14,8 @@ object Day8 extends AdventApp[TreeGrid](year = 2022, day = 8) {
   def part2(input: Input): IO[Any] = input.part2
 }
 
+case class Pos(x: Int, y: Int)
 case class TreeGrid(grid: Vector[Vector[Int]]) { self =>
-  override def toString = grid
-    .map(_.map {
-      case -1    => '+'
-      case other => other.toString().head
-    }.mkString)
-    .mkString("\n")
-
   lazy val allPos = for {
     y <- (0 until rows).toVector
     x <- (0 until cols)
@@ -30,49 +24,40 @@ case class TreeGrid(grid: Vector[Vector[Int]]) { self =>
   val rows = grid.length
   val cols = grid.head.length
 
-  val part1: IO[Int] =
-    allPos
-      .parTraverse(pos => IO(visibleFromOutside(pos)))
-      .map(_.count(identity))
-  val part2: IO[Int] =
-    allPos.parTraverse(pos => IO(visibilityInside(pos))).map(_.reduce(_ max _))
+  val part1: IO[Int] = allPos.parTraverse(isVisibleFromOutside).map(_.count(identity))
+  val part2: IO[Int] = allPos.parTraverse(visibilityInside).map(_.reduce(_ max _))
 
   def lookup(pos: Pos): Int = {
     for {
-      row <- grid.lift(pos.y)
+      row  <- grid.lift(pos.y)
       tree <- row.lift(pos.x)
     } yield tree
   } getOrElse -1
 
-  def calculate(
-      lineReduce: List[Int] => Int
-  )(
-      directionReduce: (Int, Int) => Int
-  )(pos: Pos): Int = Vector(
+  def calculate(pos: Pos)(treeLineReduce: List[Int] => Int)(directionReduce: (Int, Int) => Int): Int = List(
     (pos.x to -1 by -1).toList.drop(1).map(x => lookup(Pos(x, pos.y))),
     (pos.y to -1 by -1).toList.drop(1).map(y => lookup(Pos(pos.x, y))),
     (pos.x to cols).toList.drop(1).map(x => lookup(Pos(x, pos.y))),
     (pos.y to rows).toList.drop(1).map(y => lookup(Pos(pos.x, y)))
-  ).map(lineReduce).reduce(directionReduce)
+  ).map(treeLineReduce).reduce(directionReduce)
 
-  def visibleFromOutside(pos: Pos): Boolean = {
+  def isVisibleFromOutside(pos: Pos): IO[Boolean] = IO {
     val treeHeight = lookup(pos)
-    val minHeight = calculate(_.reduce(_ max _))(_ min _)(pos)
+    val minHeight  = calculate(pos)(_.reduce(_ max _))(_ min _)
     treeHeight > minHeight
   }
 
-  def visibilityInside(pos: Pos): Int = {
+  def visibilityInside(pos: Pos): IO[Int] = IO {
     val treeHeight = lookup(pos)
-    calculate(
-      _.dropRight(1)
-        .foldLeft[Either[Int, Int]](Left(0)) {
-          case (Right(acc), _)                             => Right(acc)
-          case (Left(acc), height) if height >= treeHeight => Right(acc + 1)
-          case (Left(acc), height)                         => Left(acc + 1)
-        }
-        .fold(identity, identity)
-    )(_ * _)(pos)
+    def viewDistance(trees: List[Int]) = trees
+      .dropRight(1)
+      .foldLeft[Either[Int, Int]](Left(0)) {
+        case (Right(acc), _)                             => Right(acc)
+        case (Left(acc), height) if height >= treeHeight => Right(acc + 1)
+        case (Left(acc), height)                         => Left(acc + 1)
+      }
+      .fold(identity, identity)
+
+    calculate(pos)(viewDistance)(_ * _)
   }
 }
-
-case class Pos(x: Int, y: Int)
